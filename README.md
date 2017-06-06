f8b8ace419621a07dcd393e47a36c45449159ef6
Ian McFarlan <ian@mcfarlan.ca>
2017-06-06 16:44:04 -0400

    start of OOP build

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PDF Tables for PHP
 
-Send PDF documents to [PDF Tables](https://pdftables.com/), via their [API](https://pdftables.com/pdf-to-excel-api) to convert, into CSV, XML, or Excel format. Setup is configured for a Mac environment out-of-box but should work on Linux installs and Windows 10 installs that have the [Creators Update](https://blogs.windows.com/windowsexperience/2017/03/30/managing-windows-10-creators-update-rollout-seamless-experience/#49I5Q8vtTXlsJm4W.97) installed (for *nix command line).
+Send PDF documents to [PDF Tables](https://pdftables.com/), via their [API](https://pdftables.com/pdf-to-excel-api) to convert, into CSV, XML, or Excel format. Setup is configured for a Mac environment out-of-box but should work on Linux installs and Windows 10 installs that have the [Creators Update](https://blogs.windows.com/windowsexperience/2017/03/30/managing-windows-10-creators-update-rollout-seamless-experience/#49I5Q8vtTXlsJm4W.97) installed (for \*nix command line).
 
 ## Installation
 
--- convert.php
+++ convert.php
@@ -1,75 +1,113 @@
 <?php
 
-$dir = __DIR__ . '/';
-require_once $dir . 'config.php';
+/**
+ * Converts PDFs to CSVs, XML, or XLSX
+ */
+class PDF_Tables_PHP {
 
-$files = array_diff( scandir( __DIR__ ), $ignore_these_files );
+    private $api_key                  = '';             // Plugin your API key from https://pdftables.com/pdf-to-excel-api
+    private $format                   = 'csv';          // Possible formats include 'csv', 'xml', 'xlsx-single', 'xlsx-multiple'
+    private $save_conversion_here     = 'csv';          // Name of the folder you would like to save the converted file to; relative path
+    private $move_processed_pdfs_here = 'processed';    // Move all processed PDFs to this folder; relative path
 
-foreach( $files as $file ):
+    function __construct() {
+        $this->dir      =  __DIR__ . '/';
+        $this->files    = array_diff( scandir( __DIR__ ), $this->ignore_these_files() );
+    }
 
-    // Only PDFs
-    if ( substr( $file, -3 ) == 'pdf' && substr( $file, -3 != 'csv' ) ) {
+    private function ignore_these_files() {
+        $ignore_these_files   = array(
+            '..',
+            '.',
+            'convert.php',
+            'csv',
+            '.DS_Store',
+            'processed',
+            'start_conversion.command'
+        );
 
-        convert_to_csv( $file );
+        return $ignore_these_files;
+    }
 
-        if ( copy( $file, 'processed/'. $file ) ) {
-            unlink( $file );
+    private function which_format_ext( $format ) {
+        if ( $format == 'csv' || $format == 'xml' ) {
+            return $format;
+        } else if ( $format == 'xlsx-single' || $format == 'xlsx-multiple' ) {
+            return 'xlsx';
+        }
+    }
+
+    private function convert_a_file( $file ) {
+        $the_curl   = curl_init();
+        $filename   = substr( $file, 0, -4 );
+        $file       = curl_file_create( $file, 'application/pdf' );
+        $format_ext = which_format_ext( $format );
+
+        curl_setopt( $the_curl, CURLOPT_URL, 'https://pdftables.com/api?key=' . $api_key . '&format=' . $format );
+        curl_setopt( $the_curl, CURLOPT_POSTFIELDS, array( 'file' => $file ) );
+        curl_setopt( $the_curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $the_curl, CURLOPT_ENCODING, 'gzip,deflate' );
+
+        $result = curl_exec( $the_curl );
+        if ( curl_errno( $the_curl ) ) {
+            print( 'Oh Dear! Error calling PDFTables: ' . curl_error( $the_curl ) );
         }
 
+        // Save PDF converstion to file
+        file_put_contents ( $filename . '.' . $format_ext , $result );
+
+        // Close the connection
+        curl_close( $the_curl );
+
+        // Relax the connection to allow for latency
+        sleep( 2 );
+
+        // Move the converted file into the processed folder
+        if ( copy( $filename . '.' . $format_ext, $save_conversion_here . '/' . $filename . '.' . $format_ext ) ) {
+            unlink( $filename . '.' . $format_ext );
+        }
     }
 
-endforeach;
+    private function maybe_convert_file( $file ) {
+        if ( substr( $file, -3 ) == 'pdf' && substr( $file, -3 != 'csv' ) ) {
+            convert_to_csv( $file );
 
-if ( $api_key !== '' ) {
-    return $api_key;
+            if ( copy( $file, 'processed/'. $file ) ) {
+                unlink( $file );
+            }
+        }
+    }
+
+    private function convert_to_csv( $file ) {
+        $the_curl   = curl_init();
+        $filename   = substr( $file, 0, -4 );
+        $file       = curl_file_create( $file, 'application/pdf' );
+        $format_ext = $this->which_format_ext( $this->format );
+
+        curl_setopt( $the_curl, CURLOPT_URL, 'https://pdftables.com/api?key=' . $api_key . '&format=' . $this->format );
+        curl_setopt( $the_curl, CURLOPT_POSTFIELDS, array( 'file' => $file ) );
+        curl_setopt( $the_curl, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $the_curl, CURLOPT_ENCODING, 'gzip,deflate' );
+
+        $result = curl_exec( $the_curl );
+        if ( curl_errno( $the_curl ) ) {
+            print( 'Oh Dear! Error calling PDFTables: ' . curl_error( $the_curl ) );
+        }
+
+        // Save PDF converstion to file
+        file_put_contents ( $filename . '.' . $format_ext , $result );
+
+        // Close the connection
+        curl_close( $the_curl );
+
+        // Relax the connection to allow for latency
+        sleep( 2 );
+
+        // Move the converted file into the processed folder
+        if ( copy( $filename . '.' . $format_ext, $save_conversion_here . '/' . $filename . '.' . $format_ext ) ) {
+            unlink( $filename . '.' . $format_ext );
+        }
+    }
 }
 
-/**
- * Sends a PDF to PDF Tables API for conversion
- *
- * @param string    $file
- */
-function convert_to_csv( $file ) {
-    $the_curl   = curl_init();
-    $filename   = substr( $file, 0, -4 );
-    $file       = curl_file_create( $file, 'application/pdf' );
-    $format_ext = which_format_ext( $format );
-
-    curl_setopt( $the_curl, CURLOPT_URL, 'https://pdftables.com/api?key=' . $api_key . '&format=' . $format );
-    curl_setopt( $the_curl, CURLOPT_POSTFIELDS, array( 'file' => $file ) );
-    curl_setopt( $the_curl, CURLOPT_RETURNTRANSFER, true );
-    curl_setopt( $the_curl, CURLOPT_ENCODING, 'gzip,deflate' );
-
-    $result = curl_exec( $the_curl );
-    if ( curl_errno( $the_curl ) ) {
-        print( 'Oh Dear! Error calling PDFTables: ' . curl_error( $the_curl ) );
-    }
-
-    // Save PDF converstion to file
-    file_put_contents ( $filename . '.' . $format_ext , $result );
-
-    // Close the connection
-    curl_close( $the_curl );
-
-    // Relax the connection to allow for latency
-    sleep( 2 );
-
-    // Move the converted file into the processed folder
-    if ( copy( $filename . '.' . $format_ext, $save_conversion_here . '/' . $filename . '.' . $format_ext ) ) {
-        unlink( $filename . '.' . $format_ext );
-    }
-
-}
-
-/**
- * Determine the file extension to save the conversion
- *
- * @param string    $format
- */
-function which_format_ext( $format ) {
-    if ( $format == 'csv' || $format == 'xml' ) {
-        return $format;
-    } else if ( $format == 'xlsx-single' || $format == 'xlsx-multiple' ) {
-        return 'xlsx';
-    }
-}
+PDF_Tables_PHP->run();
